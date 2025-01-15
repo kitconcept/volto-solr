8332b5d42f2f8c8bc291af6c1a8d88c1e18e86ab
Balázs Reé <ree@kitconcept.com>
2025-01-15 11:45:44 +0100

    WIP

--- packages/volto-solr/src/actions/solrsearch/solrSearchSuggestions.js
+++ packages/volto-solr/src/actions/solrsearch/solrSearchSuggestions.js
@@ -1,11 +1,134 @@
-export const GET_SOLR_SEARCH_SUGGESTIONS = 'GET_SOLR_SEARCH_SUGGESTIONS';
+import {
+  compact,
+  concat,
+  isArray,
+  join,
+  map,
+  pickBy,
+  toPairs,
+  identity,
+} from 'lodash';
+import config from '@plone/volto/registry';
 
-export function solrSearchSuggestions(term) {
+export const SOLR_SEARCH_SUGGESTIONS = 'SOLR_SEARCH_SUGGESTIONS';
+export const RESET_SOLR_SEARCH_SUGGESTIONS = 'RESET_SOLR_SEARCH_SUGGESTIONS';
+
+/**
+ * Search content function.
+ * @function solrSearchSuggestions
+ * @param {string} url Url to use as base.
+ * @param {Object} options Search options.
+ * @param {string} subrequest Key of the subrequest.
+ * @returns {Object} Search suggestions action.
+ */
+export function solrSearchSuggestions(url, options, subrequest = null) {
+  let queryArray = [];
+  options = pickBy(options, identity);
+  const { settings } = config;
+
+  const arrayOptions = pickBy(options, (item) => isArray(item));
+
+  // XXX Note: The `review_state` term is
+  // currently ignored by the `@@solr` backend service. However
+  // its implementation on the front-end is ready.
+
+  const pathPrefix =
+    (options.local || '').toLowerCase() === 'true' && options.path_prefix;
+
+  const emptySearchCondition =
+    !options.SearchableText &&
+    !options['portal_type'] &&
+    !options['review_state'];
+
+  if (!options.doEmptySearch && emptySearchCondition) {
+    // If none of the conditions are specified, we don't do a server
+    // search but return an empty result set in a shortcut.
+    // Note that an empty `q` parameter would fail anyway.
+    // This behavior is configurable by the `doEmptySearch` option.
+    return resetSolrSearchSuggestions(subrequest);
+  }
+
+  queryArray = concat(
+    queryArray,
+    options
+      ? join(
+          map(toPairs(pickBy(options, (item) => !isArray(item))), (item) => {
+            if (item[0] === 'b_start') {
+              // Solr service uses start
+              item[0] = 'start';
+            } else if (
+              [
+                'b_size',
+                // 'sort_on',
+                // 'sort_order',
+                'SearchableText',
+                'local',
+                'path_prefix',
+              ].includes(item[0])
+            ) {
+              // Injected later
+              return;
+            }
+            // Urlencoding needed
+            const [k, v] = item;
+            return `${k}=${encodeURIComponent(v)}`;
+          }).filter((e) => e),
+          '&',
+        )
+      : '',
+    arrayOptions
+      ? join(
+          map(pickBy(arrayOptions), (item, key) => {
+            if (key === 'metadata_fields') {
+              // Solr service does not support it
+              return '';
+            } else {
+              return join(
+                item.map((value) => `${key}:list=${value}`),
+                '&',
+              );
+            }
+          }),
+          '&',
+        )
+      : '',
+    `q=${options.SearchableText ?? ''}`,
+    // Default batch size is injected here
+    `rows=${
+      options.b_size !== undefined ? options.b_size : settings.defaultPageSize
+    }`,
+    pathPrefix ? `path_prefix=${options.path_prefix}` : '',
+    // options.sort_on !== undefined
+    //   ? 'sort=' +
+    //       encodeURIComponent(
+    //         `${options.sort_on} ${
+    //           options.sort_order === 'reverse' ? 'desc' : 'asc'
+    //         }`,
+    //       )
+    //   : '',
+  );
+
+  const querystring = join(compact(queryArray), '&');
+
   return {
-    type: GET_SOLR_SEARCH_SUGGESTIONS,
+    type: SOLR_SEARCH_SUGGESTIONS,
+    subrequest,
     request: {
       op: 'get',
-      path: `/@solr-suggest?query=${term}`,
+      path: `${url}/@solr-suggest${querystring ? `?${querystring}` : ''}`,
     },
+  };
+}
+
+/**
+ * Reset search content function.
+ * @function resetSolrSearchSuggestions
+ * @param {string} subrequest Key of the subrequest.
+ * @returns {Object} Search suggestions action.
+ */
+export function resetSolrSearchSuggestions(subrequest = null) {
+  return {
+    type: RESET_SOLR_SEARCH_SUGGESTIONS,
+    subrequest,
   };
 }
--- packages/volto-solr/src/reducers/solrsearch/solrSearchSuggestions.js
+++ packages/volto-solr/src/reducers/solrsearch/solrSearchSuggestions.js
@@ -1,37 +1,65 @@
-import { GET_SOLR_SEARCH_SUGGESTIONS } from '../../actions/solrsearch/solrSearchSuggestions';
+import { map, omit } from 'lodash';
+import {
+  SOLR_SEARCH_SUGGESTIONS,
+  RESET_SOLR_SEARCH_SUGGESTIONS,
+} from '../../actions/solrsearch/solrSearchSuggestions';
 
 const initialState = {
   error: null,
   items: [],
   loaded: false,
   loading: false,
+  batching: {},
+  subrequests: {},
 };
 
 export default function searchSuggestions(state = initialState, action = {}) {
   switch (action.type) {
-    case `${GET_SOLR_SEARCH_SUGGESTIONS}_PENDING`:
+    case `${SOLR_SEARCH_SUGGESTIONS}_PENDING`:
       return {
         ...state,
         error: null,
+        total: 0,
+        loading: false,
         loaded: false,
-        loading: true,
+        batching: {},
       };
-    case `${GET_SOLR_SEARCH_SUGGESTIONS}_SUCCESS`:
+    case `${SOLR_SEARCH_SUGGESTIONS}_SUCCESS`:
       return {
         ...state,
         error: null,
         items: action.result.suggestions,
-        loaded: true,
+        total: 0, // XXX TBD
         loading: false,
+        loaded: false,
+        batching: {},
       };
-    case `${GET_SOLR_SEARCH_SUGGESTIONS}_FAIL`:
+    case `${SOLR_SEARCH_SUGGESTIONS}_FAIL`:
       return {
         ...state,
         error: action.error,
         items: [],
-        loaded: false,
+        total: 0,
         loading: false,
+        loaded: false,
+        batching: {},
       };
+    case RESET_SOLR_SEARCH_SUGGESTIONS:
+      return action.subrequest
+        ? {
+            ...state,
+            subrequests: omit(state.subrequests, [action.subrequest]),
+          }
+        : {
+            ...state,
+            error: null,
+            items: [],
+            total: 0,
+            loading: false,
+            loaded: false,
+            batching: {},
+          };
+
     default:
       return state;
   }
